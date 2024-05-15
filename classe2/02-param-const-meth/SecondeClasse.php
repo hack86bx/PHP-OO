a3ed5ec2bda3c64852c945b8cfbf37060d07340d
WebDevCF2m <gitweb@cf2m.be>
2024-05-15 10:30:34 +0200

    02-param-const-meth

---
+++ classe2/02-param-const-meth/SecondeClasse.php
@@ -0,0 +1,12 @@
+<?php
+class SecondeClasse{
+    /*
+     Paramètres
+     Se sont les variables de notre classe
+     Nous verrons la visibilité plus loin
+     Nous les mettons en public par défaut
+    */
+    public $param1;
+    public $param2="coucou";
+    public int $param3; // on peut typer les paramètres (devrait !)
+}
---
+++ classe2/02-param-const-meth/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once "SecondeClasse.php";
+
+$instance1 = new SecondeClasse();
+
+var_dump($instance1);
