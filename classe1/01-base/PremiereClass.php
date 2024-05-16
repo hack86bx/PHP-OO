074514cf0b41176f84760884122c4105c6e6955d
WebDevCF2m <gitweb@cf2m.be>
2024-05-16 10:26:39 +0200

    01

---
+++ classe1/01-base/PremiereClass.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+Création d'une classe en PascalCase
+Le nom de la classe doit être le nom du fichier (sans le .php)
+PSR (https://www.php-fig.org/psr/)
+https://www.php.net/manual/fr/language.oop5.php
+! Une seule classe par fichier
+
+On crée une classe avec le mot clef class, suivi du nom en PascalCase, puis {}
+
+*/
+
+class PremiereClass{
+
+}
