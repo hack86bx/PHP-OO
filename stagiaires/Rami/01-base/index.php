0a47a87515731a0c2faf020510960cdbcaaf09f3
rammroumm2K <ramybrz00@gmail.com>
2024-05-16 12:13:15 +0200

    02

--- stagiaires/Rami/01-base/index.php
+++ stagiaires/Rami/01-base/index.php
@@ -20,3 +20,20 @@ var_dump(
     $instance2, // deuxieme objet (ou instance de classe) de type PremiereClasse
     $instance3, // ceci n'est qu'un lien vers l'instance numéro 1
  );
+
+ ?>
+<h3>En PHP les array ne sont pas des objets !</h3>
+<p>On peut donc les cloner avec le signe =</p>
+<p>C'est historiquement du au fait que PHP est un langage créé pour utiliser les tableaux</p>
+<code>
+    $tab1 = [1,2,3];
+    $tab2 = $tab1;
+</code>
+<?php
+$tab1 = [1,2,3];
+$tab2 = $tab1;
+var_dump($tab1,$tab2);
+$tab1[] = 4;
+$tab2[]= "coucou";
+$tab2["lulu"]=7;
+var_dump($tab1,$tab2);
