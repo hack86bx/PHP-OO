1b3e8128413c48cc853f7a6fa1ae3358ead7afa4
hack86bx <emrah.arpaci@bftice.be>
2024-05-16 11:38:25 +0200

    160524

---
+++ stagiaires/Emrah/01-base-pooEmrah/PremiereClass.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+creation d'une classe en PascalCase
+le nom de la classe doit etre le nom du fichier (sans le .php)
+PSR (https://www.php-fig.org/psr/)
+https://www.php.net/manual/fr/language.oop5.php
+! une seule classe par fichier
+
+on cree une classe avec le mot clef class, suivi du nom PasclaCase, puis{}
+*/
+
+class PremiereClass{
+    
+}
