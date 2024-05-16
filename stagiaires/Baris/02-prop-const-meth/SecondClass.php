e7dfafdb432c2ce921a77d588a774502ded9ba7a
Helioz7 <baris.aktas@cf2000.onmicrosoft.com>
2024-05-16 11:41:16 +0200

    02

--- stagiaires/Baris/02-prop-const-meth/SecondClass.php
+++ stagiaires/Baris/02-prop-const-meth/SecondClass.php
@@ -3,26 +3,24 @@
 class SecondClass{
 
     /*
-
-    propriété
-    ce sont des variables (même règle de nommage)
+    Propriétés
+    Ce sont des variables (même règle de nommage)
     internes à la classe
-    la visibilité par défaut est publique
+    La visibilité par défaut est publique
     */
-
     public $param1; // propriété vide
     public $param2 = "coucou"; // propriété avec valeur par défaut
     public int $param3; // propriété typée
-    public int|float $param4; // propriété multi-type(php8)
-    public ? string $param5; // propriété multi-types (valide en php 7) null ou string
-    
-    
+    public int|float $param4; // propriété multi-type (PHP 8) int ou float
+    public ?string $param5; // propriété multi-type (valide en PHP 7) null ou string
 
-    /* 
-    constantes
-    ce sont des variables non modifiables qui doivent être initialisées avec leurs valeur. Les règles de nommage sont en UPPER_SNAKE_CASE 
-    la variable par défaut est publique
+    /*
+    Constantes
+    Ce sont des variable non modifiables qui doivent être initialisées
+    avec leur valeur. Les règles de nommage sont en UPPER_SNAKE_CASE
+    La visibilité par défaut est publique
     */
-
     const CONSTANTE_1 = "constante publique 1";
+
+
 }
