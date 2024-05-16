ce88ea2d9417a8d1d477c7ba21b2896497743936
Helioz7 <baris.aktas@cf2000.onmicrosoft.com>
2024-05-16 10:29:18 +0200

    01

---
+++ stagiaires/Baris/01-base/PremiereClass.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+création d'une classe en PascaleCase
+le nom de la classe doit être le nom du fichier (sans le .php) 
+
+une seule classe par fichier
+
+on crée une classe avec le mot clef class
+suivi du nom en PascaleCase,puis {}
+*/
+
+class PremiereClass{
+
+}
