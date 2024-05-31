46d29d6a90c1465e8fd963ba6d37b0ee88080445
PommeDeuxTerre <147161646+PommeDeuxTerre@users.noreply.github.com>
2024-05-31 10:55:05 +0200

    pomme

---
+++ stagiaires/Gregory/01-param-const-meth/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once("SecondClass.php");
+
+$instance = new SecondClass();
+
+var_dump($instance);
---
+++ stagiaires/Gregory/02-visibility/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once("ThirdClass.php");
+
+$instance = new ThirdClass();
+echo $instance->param1;
