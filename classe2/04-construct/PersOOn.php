5870bdb36d3a0b4371e7f2805e8d41dd216c85be
WebDevCF2m <gitweb@cf2m.be>
2024-05-31 10:12:18 +0200

    04 param

--- classe2/04-construct/PersOOn.php
+++ classe2/04-construct/PersOOn.php
@@ -1,5 +1,12 @@
 <?php
 
 class PersOOn {
-    
+    /*
+     Propriétés -> variables
+     */
+    private string $especePerso;
+    private string $nomPerso;
+    protected ?int $xpPerso; // xp du personnage, peut être null ou int (?int)
+    // équivalent depuis PHP 8.0 : protected null|int $xpPerso;
+    protected null|bool|int $hpPerso; // si plus de 2 types, utilisation des pipes
 }
