515f119dc32a7d9264563222ae199d08e656fa06
Maxi Rojas <arnaldomrojas@outlook.com>
2024-05-31 10:12:35 +0200

    construct

--- stagiaires/Maxi/04-construct/PersOOn.php
+++ stagiaires/Maxi/04-construct/PersOOn.php
@@ -1,5 +1,11 @@
 <?php
 
 class PersOOn {
+    //propriétés -> variables
+    private string $especePerso;
+    private string $nomPerso;
+    protected ?int $xpPerso; // xp du personnag, peut être null ou int (?int)
+    //equivalent despuis PHP 8.0 : protected null|int $xpPerso;
+    protected null|bool|string|int $hpPerso; //si plus de 2 types, utilisation des pipes (|)
     
 }
--- stagiaires/Maxi/04-construct/index.php
+++ stagiaires/Maxi/04-construct/index.php
@@ -1,1 +1,10 @@
 <?php
+require_once "PersOOn.php";
+
+// création d'une instance de la classe PersOOn
+$perso1 = new PersOOn();
+
+//var_dump($perso1);
+
+
+
