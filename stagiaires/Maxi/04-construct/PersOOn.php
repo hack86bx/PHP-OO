1ecebbb6c9a80b8e6286d4698cd82e2e7f5d8045
Maxi Rojas <arnaldomrojas@outlook.com>
2024-05-31 10:57:22 +0200

    construct

--- stagiaires/Maxi/04-construct/PersOOn.php
+++ stagiaires/Maxi/04-construct/PersOOn.php
@@ -9,7 +9,7 @@ class PersOOn {
     protected null|bool|string|int $hpPerso; //si plus de 2 types, utilisation des pipes (|)
 
 
-    //constantes -> constantes
+    //constantes -> équivalent constantes
     //les especes que seront acceptéesdans le jeu
     public const ESPECE_PERSO = [
         "Humain",
@@ -19,4 +19,17 @@ class PersOOn {
         "Hobbit",
         "Gobelin",
     ];
+    //methodes -> equivalent des fontcations
+
+    //methode magique : constructeur
+    //c'est une methode publice qui est appelée lors de l'instanciation de la classe donc l'utilisation du mot clé new.
+    //Elle permet de passer des paramètres lors de la creation de la instance.
+    public function __construct(string $name, string $species, ?int $xp = null, null|bool|string|int $hp =100) {
+        //on peut remplir les paramètres (mauvaise pratique)
+        $this->nomPerso = $name;
+        $this->especePerso = $species;
+        $this->xpPerso = $xp;
+        $this->hpPerso = $hp;
+
+    }
 }
