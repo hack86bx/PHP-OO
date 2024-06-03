211e244c2f4e35884bfd70bd96a39d9cad88d210
Anthoonyyy <Anthony212007@hotmail.com>
2024-06-03 16:14:53 +0200

    correction exe6

--- stagiaires/Anthony/06-extends/PersoMagicien.php
+++ stagiaires/Anthony/06-extends/PersoMagicien.php
@@ -1,53 +1,37 @@
 <?php
 
 class PersoMagicien extends PersoBase{
+    // Propriétés (non héritées) - ajout
+    # créer un getter et un setter
+    protected ?int $magiePoint;
 
-    // Propriétés (non héritée) - ajout
-    # Créer un getter et un setter
-         protected ?int $magiePoint;
+    // Propriétés écrasées (public ou protégées) - remplacement
+    protected int $force = 50;
 
+    // On ne peut pas l'écraser une propriété privée du parent !
+    // public int|bool|null $alive = 5;
 
-    // Propriétés écrasées (public ou protected) - remplacement
-         protected int $force = 50;
+    // méthodes
 
+    // Surcharge du constructeur on peut ajouter des éléments
+    public function __construct(string $name, int $age, string $espece){
+        // on garde le constructeur du parent
+        parent::__construct($name,$age,$espece);
+        // on ajoute ce que l'on souhaite au constructeur de l'enfant
+        # création du setter et appel ici
+        $this->setMagiePoint(100);
+    }
 
-    // On ne peut pas  écraser une propriété privée du parent 
-
-        // private int|bool|null $alive =5;
-
-
-    // Méthodes
-
-    // getter de $magiePoint (int)
+    // getter de magiePoint
     public function getMagiePoint(): ?int
     {
-    return $this->magiePoint;
-    }   
+        return $this->magiePoint;
+    }
 
-     // setter de magiePoint
-     public function setMagiePoint(int $magiePoint){
-       
-            $this->magiePoint = $magiePoint;
-        
+    // setter de magiePoint
+    public function setMagiePoint(int $themagiePoint){
+        $this->magiePoint = $themagiePoint;
     }
 
     
-    // surcharge du constructeur, on peut ajouter des éléments
-    public function __construct( string $name, int $age, string $espece, int $magiePoint)
-    {
-        // on garde le constructeur du parent
-      parent::__construct( $name,  $age,  $espece );
-      // on ajoute ce que l'on souhaite au constructeur de l'enfant 
-
-      #création du setter
-      $this->setMagiePoint($magiePoint);
-    }    
-    
-   
-
 }
-
-
-
-
-  
