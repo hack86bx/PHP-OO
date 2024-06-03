30cb4337d1827f5d1e4c5d7e11d9e2b37006ab4b
hack86bx <emrah.arpaci@bftice.be>
2024-06-03 13:15:51 +0200

    030624

--- stagiaires/Emrah/02-prop-const-meth/index.php
+++ stagiaires/Emrah/02-prop-const-meth/index.php
@@ -2,6 +2,7 @@
 
 require_once "SecondeClass.php";
 
+// instanciation d'un objet de type SecondeClass
 $instance1 = new SecondeClass();
 
 var_dump(
@@ -36,5 +37,17 @@ echo SecondeClass::CONSTANTE_1; // bonne pratique
 echo $instance1::CONSTANTE_1; // mauvaise pratique
 echo "<br>";
 echo SecondeClass::CONSTANTE_1; // bonne pratique
+
+?>
+<h3>Methodes publiques</h3>
+<p>Ces méthodes peuvent être appelées en dehors (comme en dedans) de l'instance avec l'opérateur -></p>
+<code>$instance1->bonjourLesAmies();</code>
+<?php
+$instance1->bonjourLesAmies(); // affiche (donc mauvaise pratique)
+?><br>
+<code>echo $instance1->getParam2(); // getter valide</code>
+<?php
+echo $instance1->getParam2(); // getter valide
+
 
 
