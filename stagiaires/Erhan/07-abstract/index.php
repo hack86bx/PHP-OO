e2118138f1676cc3d6652193548045e19ff37555
erdaserhan <erhan209@gmail.com>
2024-06-03 16:17:23 +0200

    abstract

---
+++ stagiaires/Erhan/07-abstract/index.php
@@ -0,0 +1,22 @@
+<?php
+require_once "PersoAbstract.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Class Abstract</title>
+</head>
+<body>
+    <h1>Class Abstract</h1>
+    <p>Une classe abstraite ne peut être instanciée, elle va servir de modèle pour les classes enfants</p><p>On peut hériter des propriétés ou méthodes non abstraites.</p>
+    <pre><code></code></pre>
+    <?php
+    //$perso = new PersoAbstract();
+    ?>
+    
+</body>
+</html>
+<?php
+
