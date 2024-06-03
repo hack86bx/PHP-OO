1058792ecffa81e7fed4497f55af7f5e22a53dc4
Anthoonyyy <Anthony212007@hotmail.com>
2024-06-03 16:15:28 +0200

    07-abstract

---
+++ stagiaires/Anthony/07-abstract/PersoAbstract.php
@@ -0,0 +1,9 @@
+<?php
+
+abstract class PersoAbstract {
+
+    // Propriétés
+
+     
+
+}
---
+++ stagiaires/Anthony/07-abstract/index.php
@@ -0,0 +1,20 @@
+<?php
+require_once "PersoAbstract.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>class abstract</title>
+</head>
+<body>
+    <h1>Class abstract</h1>
+    <p>Une classe abstraite ne peut être instanciée, elle va servir de modèle pour les classes enfants</p>
+    <p>On peut par contre hériter des propriétées ou méthodes non-abstraite </p>
+    <code> // $perso = new PersoAbstract();</code>
+    <?php
+  
+    ?>
+</body>
+</html>
