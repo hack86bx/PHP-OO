a5848a22e411002ffc3741f85c4881aaa81aa0d1
rammroumm2K <ramybrz00@gmail.com>
2024-06-04 13:40:01 +0200

    454545

--- stagiaires/Rami/07-abstract/index.php
+++ stagiaires/Rami/07-abstract/index.php
@@ -9,10 +9,9 @@
     <h1>class abstract</h1>
     <p>Une classe abstraite ne peut être instanciée, elle va servir de modèle pour les classes enfants</p>
     <p>On peut hériter des propriétées ou méthodesq non abstraites</p>
-    <?php
-    <code>$perso= new PersoAbstract();
-</code> 
     
-    ?>
+    <code>$perso= new PersoAbstract()</code> 
+    
+    
 </body>
 </html>
