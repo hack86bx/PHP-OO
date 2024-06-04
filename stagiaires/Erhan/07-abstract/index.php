e67cf90df4a424e6f3810a5d2f2233fb4f98911a
erdaserhan <erhan209@gmail.com>
2024-06-04 15:04:23 +0200

    enemy

--- stagiaires/Erhan/07-abstract/index.php
+++ stagiaires/Erhan/07-abstract/index.php
@@ -1,5 +1,6 @@
 <?php
 require_once "PersoAbstract.php";
+require_once "PersoWarrior.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,12 +12,22 @@ require_once "PersoAbstract.php";
 <body>
     <h1>Class Abstract</h1>
     <p>Une classe abstraite ne peut être instanciée, elle va servir de modèle pour les classes enfants</p><p>On peut hériter des propriétés ou méthodes non abstraites.</p>
-    <pre><code></code></pre>
+    <pre><code>//$perso = new PersoAbstract();</code></pre>
+    <p>On peut par contre afficher une constante publique</p>
+    <code>//var_dump(PersoAbstract::ESPECE_CHOICE);</code>
     <?php
     //$perso = new PersoAbstract();
+    var_dump(PersoAbstract::ESPECE_CHOICE);
+    ?>
+    <h3>Un héritier doit créer les méthodes abstraites</h3>
+    <code>$persoWarrior1 = new PersoWarrior("Luc");</code>
+    <?php
+    $persoWarrior1 = new PersoWarrior("Luc", "Humain");
+    $persoWarrior1->setHealthPoint(30);
+    echo $persoWarrior1->getHealthPoint();
+
+    var_dump($persoWarrior1);
     ?>
     
 </body>
 </html>
-<?php
-
