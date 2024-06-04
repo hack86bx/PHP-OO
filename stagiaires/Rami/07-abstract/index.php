c8b8a3f021d00f3bc45d09031734425a24d534a2
rammroumm2K <ramybrz00@gmail.com>
2024-06-04 13:17:56 +0200

    correction html

--- stagiaires/Rami/07-abstract/index.php
+++ stagiaires/Rami/07-abstract/index.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Class Abst</title>
 </head>
-<body style="background-color: grey; color: ;">
+<body style="background-color: grey;">
     <h1>class abstract</h1>
     <p>Une classe abstraite ne peut être instanciée, elle va servir de modèle pour les classes enfants</p>
     <p>On peut hériter des propriétées ou méthodesq non abstraites</p>
     <?php
-    $perso= new PersoAbstract();
+    //$perso= new PersoAbstract();
 
     
     ?>
