1433cc1277c066f79d9232ae175110c8ff56bc34
mikhawa <michaeljpitz@gmail.com>
2024-06-09 19:48:42 +0200

    classe 2 abstract class

---
+++ classe2/07-abstract/PersoClasse2Abstract.php
@@ -0,0 +1,17 @@
+<?php
+
+abstract class PersoClasse2Abstract
+{
+
+    /*
+     * Propriétés
+     */
+
+    // Propriétés héritables
+    protected ?string $persoName;
+    protected ?string $persoEspece;
+    protected ?string $infoPerso;
+    protected int $persoXP = 0;
+    protected int $persoHP = 1000;
+    protected int $persoAbility = 100;
+}
